33dd874e9af6e4c3c6bd8a701fd96ad5abc7dae6
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2023-09-08 15:03:33 +0600

    demo.py

--- demo2.py
+++ demo2.py
@@ -1,7 +1,7 @@
 from req_module import *
 
 db=Request_Firebase(project_id='requestfirebase108')
-#give your project id, make sure all rules are >> 'true'   just replace everything says false
+
 
 
 #db.input_data(path=f'10_06_2023',data={99998:{'name':'t iqbal','proffession':'house wife','dieasese':'kidney','doctor name':'ziauddin'}})
@@ -10,10 +10,12 @@ db=Request_Firebase(project_id='requestfirebase108')
 #db.delete_data(path=f"{151110+i}")
 
 
-sdb=Request_Firebase_Storage(project_id='requestfirebase108')
+sdb=Request_Firebase_Storage()
 
-#sdb.upload_file(path='js',file_name='demo.html')
-#sdb.download_files(path='js')
-#sdb.delete_files(path='js')
+#sdb.upload_file(path='py',attribute='condition',file_name='C:\\Users\\Hasnat\\Desktop\\desk\\pyscript\\Request Firebase Module\\pyscr\\webApp.py')
+#sdb.download_files(path='js',attribute='condition')
+#sdb.delete_files(path='py',attribute='name')
+#sdb.delete_files(path='py',delete_path=True)
+
 #my_db_folders=sdb.folder_list()
 #print(my_db_folders)
